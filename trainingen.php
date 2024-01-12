080a52fe8ec83e3df9a246e72cf0150b066215ea
Geert van der Velden <1079252@hr.nl>
2024-01-12 14:01:08 +0100

    trainingen naar php

--- trainingen.php
+++ trainingen.php
@@ -1,24 +1,72 @@
 <?php
 ?>
-<!doctype html>
+<!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
     <link rel="stylesheet" href="css/style.css">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>trainingen</title>
+
 </head>
+
 <body>
 
-<nav>
-    <a href="home.php">Home</a>
-    <a href="actueel.php">actueel</a>
-    <a href="bezoek.php">bezoek</a>
-    <a href="trainingen.php">trainingen</a>
-    <a href="info.php">info</a>
+<nav class=sidenav>
+    <img src="https://cubebouldergym.nl/templates/unitrii-et/images/logo.png" alt="logo" id="logo">
+    <div class=sidenavContainer>
+        <section>
 
-    <a href="login.php">medewerker</a>
-
+            <div>
+                <h2><a href="bezoek.html" class="navlinks">Home</a></h2>
+            </div>
+            <div>
+                <h2><a href="actueel.html" class="navlinks">Actueel</a></h2>
+            </div>
+            <div>
+                <h2><a href="bezoek.php" class="navlinks">Bezoek</a></h2>
+            </div>
+            <div>
+                <h2><a href="trainingen.html" class="navlinks">Trainingen</a></h2>
+            </div>
+            <div>
+                <h2><a href="info.html" class="navlinks">Info</a></h2>
+            </div>
+        </section>
+        <section class=login>
+            medewerkers
+        </section>
+    </div>
 </nav>
+
+
+
+
+<div class=container>
+    <header>
+
+    </header>
+
+    <main>
+        <h1>dit is h1</h1>
+        <h2>dit is h2</h2>
+        <h3>dit is h3</h3>
+        <h4>dit is h4</h4>
+        <p>dit is een paragraaf
+        </p>
+
+    </main>
+
+    <footer>
+        <p>sdkajhdkajsh</p>
+    </footer>
+
+</div>
+
+
+
+
+</body>
+
+</html>
