a4f8160e07a42b93e7220d944abd873109eb88bd
Jake-HBH <jakehersbach2005@gmail.com>
2024-01-18 11:44:53 +0100

    Over Ons en Openingstijden pagina aangemaakt met HTML

---
+++ openingstijden.php
@@ -0,0 +1,126 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <main>
+        <h2>Openingstijden</h2>
+        <table>
+            <tr>
+                <th>Dag</th>
+                <th>Van</th>
+                <th>Tot</th>
+            </tr>
+            <tr>
+                <td>Maandag</td>
+                <td>10:00</td>
+                <td>22:00</td>
+            </tr>
+            <tr>
+                <td>Dinsdag</td>
+                <td>10:00</td>
+                <td>22:00</td>
+            </tr>
+            <tr>
+                <td>Woensdag</td>
+                <td>10:00</td>
+                <td>22:00</td>
+            </tr>
+            <tr>
+                <td>Donderdag</td>
+                <td>10:00</td>
+                <td>22:00</td>
+            </tr>
+            <tr>
+                <td>Vrijdag</td>
+                <td>10:00</td>
+                <td>22:00</td>
+            </tr>
+            <tr>
+                <td>Zaterdag</td>
+                <td>10:00</td>
+                <td>22:00</td>
+            </tr>
+            <tr>
+                <td>Zondag</td>
+                <td>10:00</td>
+                <td>22:00</td>
+            </tr>
+        </table>
+        <h2>Afwijkende openingstijden 2024</h2>
+        <h3>Tijdens de feestdagen hanteren we soms aangepaste openingstijden. Deze staan hieronder vermeld. Op de niet vermelde dagen gelden de reguliere openingstijden.</h3>
+
+        <h3>Wij organiseren soms evenementen waarbij alleen deelnemers toegang hebben tot de klimwanden. Niet deelnemers kunnen op dat moment niet boulderen. Soms is dat een deel van de dag, soms de hele dag. Publiek is altijd gratis welkom bij deze evenementen.</h3>
+        <table>
+            <tr>
+                <th>Dag</th>
+                <th>Reden</th>
+                <th>Openingstijden</th>
+            </tr>
+            <tr>
+                <td>16 tot 19 januari</td>
+                <td>Tapijt op mat leggen</td>
+                <td>17:00 - 22:00</td>
+            </tr>
+            <tr>
+                <td>4 februari</td>
+                <td>Bedrijfsuitje</td>
+                <td>10:00 - 16:00</td>
+            </tr>
+            <tr>
+                <td>10 april</td>
+                <td>2e paasdag</td>
+                <td>14:00-22:00</td>
+            </tr>
+            <tr>
+                <td>27 april</td>
+                <td>Koningsdag</td>
+                <td>10:00-22:00</td>
+            </tr>
+            <tr>
+                <td>29 mei</td>
+                <td>2e pinksterdag</td>
+                <td>14:00-22:00</td>
+            </tr>
+            <tr>
+                <td>29 juni</td>
+                <td>Voorbereidingen IceCubes</td>
+                <td>Gesloten</td>
+            </tr>
+            <tr>
+                <td>30 juni</td>
+                <td>Voorbereidingen IceCubes</td>
+                <td>Gesloten</td>
+            </tr>
+            <tr>
+                <td>1 juli</td>
+                <td>IceCubes</td>
+                <td>Gesloten voor niet deelnemers</td>
+            </tr>
+            <tr>
+                <td>25 december</td>
+                <td>1e Kerstdag</td>
+                <td>10:00 - 19:00</td>
+            </tr>
+            <tr>
+                <td>26 december</td>
+                <td>2e Kerstdag</td>
+                <td>10:00 - 19:00</td>
+            </tr>
+            <tr>
+                <td>31 december</td>
+                <td>Oudjaarsdag</td>
+                <td>10:00 - 19:00</td>
+            </tr>
+            <tr>
+                <td>1 januari</td>
+                <td>Nieuwjaarsdag</td>
+                <td>14:00 - 19:00</td>
+            </tr>
+        </table>
+    </main>
+</body>
+</html>
---
+++ over_ons.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <main>
+        <h1>Cube Bouldergym</h1>
+        <h2>Boulderen is booming!</h2>
+        <h3>In 2011 waren we de 2e boulderhal van Nederland, klein en knus. In de jaren daarna hebben we keer op keer een kleine uitbreiding gedaan in de zomer van 2017 hebben we onze boulderwanden flink uitgebreid, en begin 2018 hebben we een geheel nieuwe kantine gemaakt. We zijn erg blij met wat we in de loop van de tijd hebben neergezet. Kom eens langs, probeer het boulderen eens, het is echt vet gaaf!</h3>
+        <h3>Boulderen is een zeer toegankelijke sport en door jong en oud te beoefenen. Cube Bouldergym beschikt over 600 m2 klimwand met 10 circuits van verschillende moeilijkheidsniveau's. In totaal zijn er meer dan 200 korte klimroutes (boulders). Er worden zeer regelmatig nieuwe boulders gebouwd door een vast team; Twee keer per week een half circuit van 10 boulders. Genoeg verse uitdagingen dus voor iedereen.</h3>
+        <h3>Cube Bouldergym beschikt over een eigen bar en verkoopt tevens kleding van NIHIL Climbing en klimschoenen van Mad Rock. Daarnaast kun je bij de Cube terecht voor accessoires zoals sporttape, chalk(bags) en crashpads.</h3>
+        <h3>Cube Bouldergym organiseert ook regionale en landelijke boulderwedstrijden en klimevenementen.</h3>
+        <h3>De boulderhal is gevestigd op het voormalige Polaroid terrein in Enschede en is één van de participanten van The Performance Factory. Binnen het concept van The Performance Factory wordt ruimte geboden aan talentontwikkeling op het gebied van onderwijs, kunst, cultuur en sport.</h3>
+    </main>
+</body>
+</html>
