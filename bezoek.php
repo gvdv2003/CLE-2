b966ee105243f1b1562d23789c639fd0954485c8
yimingeling <yiming@live.nl>
2024-01-18 13:55:49 +0100

    typfout van navbar gehaald

--- bezoek.php
+++ bezoek.php
@@ -226,7 +226,7 @@ if (isset($_POST['submit'])) {
                                 <div>Over ons</div>
                             </a>
                             <a href="openingstijden.php" class="navlinks" class="dropdown">
-                                <div>Openeningstijden</div>
+                                <div>Openingstijden</div>
                             </a>
                             <a href="contact.php" class="navlinks" class="dropdown">
                                 <div>Contact</div>
